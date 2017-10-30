2ae9caa47baf3087cfa3aea934de9577df2405ca
Sami Poimala <sami.poimala@outlook.com>
2017-10-30 15:08:31 +0200

    Initial commit

---
+++ ClassLibrary/Library.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibrary {
+	public class Library {
+		public static void Foo(IEnumerable<string> enumerable) {
+			enumerable.Append("Appended");
+		}
+	}
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp {
+	class Program {
+		static void Main(string[] args) {
+			var list = new List<string>();
+			ClassLibrary.Library.Foo(list);
+		}
+	}
+}
